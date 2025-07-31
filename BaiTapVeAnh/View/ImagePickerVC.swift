4c0f195f46c235ecb509fa77a81b2a7a68498054
Admin <admin@MacBook-Pro-cua-Admin.local>
2025-08-01 02:18:08 +0700

    update

--- BaiTapVeAnh/View/ImagePickerVC.swift
+++ BaiTapVeAnh/View/ImagePickerVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 import PhotosUI
-class ImagePickerVC: UIViewController{
+class ImagePickerVC: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate{
     @IBOutlet var addImageBtn: [UIButton]!
     @IBOutlet var chosenImageViews: [UIImageView]!
     @IBOutlet var nameTextFields: [UITextField]!
@@ -16,52 +16,51 @@ class ImagePickerVC: UIViewController{
     var numberOfImage: Int = 3
     var listImage: [UIImage] = []{
         didSet{
-            chosenImageViews.forEach { imageView in
-                imageView.image = listImage[imageView.tag]
+            for imageView in chosenImageViews {
+                if(imageView.tag < listImage.count){
+                    imageView.image = listImage[imageView.tag]
+                }
             }
         }
     }
+    private var listText: [NSAttributedString] {
+        return nameTextFields.sorted { $0.tag < $1.tag }.map({ nameTextField in
+                .init(string: nameTextField.text ?? "Khuyết danh", attributes: textAttributes)
+        })
+    }
+    
     private var finalImage: UIImage?
     
+    lazy var textAttributes: [NSAttributedString.Key: Any] = {
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        return [
+            .font: UIFont.systemFont(ofSize: 16, weight: .heavy),
+            .paragraphStyle: paragraphStyle,
+            .foregroundColor: UIColor.white
+        ]
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+            view.addGestureRecognizer(tapGesture)
     }
-//    import UIKit
-
-    // Ensure this runs on the main thread since UIKit drawing must occur on it.
-    func generateCompressedThumbnail(from finalImageView: UIImageView) -> UIImage? {
-        let size = finalImageView.bounds.size
-        let frame = CGRect(origin: .zero, size: size)
-
-        // Begin graphics context
-        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
-        finalImageView.draw(frame)
+    private func openCamera() {
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            print("❌ Thiết bị không hỗ trợ camera")
+            return
+        }
         
-        // Get image from current context
-        let renderedImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.allowsEditing = false
+        picker.delegate = self
         
-        // Compress the image to JPEG with 80% quality
-        if let renderedImage = renderedImage,
-           let jpegData = renderedImage.jpegData(compressionQuality: 0.8) {
-            return UIImage(data: jpegData)
-        } else {
-            return nil
+        DispatchQueue.main.async {
+            self.present(picker, animated: true)
         }
     }
-
-    @IBAction func add(_ sender: UIButton) {
-        var configuration = PHPickerConfiguration()
-        configuration.filter = .images
-        configuration.selectionLimit = maxImageCount
-        let picker = PHPickerViewController(configuration: configuration)
-        picker.delegate = self
-        present(picker, animated: true)
-    }
-    func drawImage(image: UIImage, frame: CGRect){
-        
-    }
-    @IBAction func createPoster(_ sender: Any) {
+    func drawImage(finalImageView: UIImageView){
         let frame = finalImageView.bounds
         let textBoundHeight:CGFloat = 24
         let width = frame.width
@@ -81,41 +80,101 @@ class ImagePickerVC: UIViewController{
             rectPath.close()
             
             rectPath.addClip()
-//            let frame2 = CGRect(x: width/4, y: 0, width: width/2, height: height)
-//            listImage[1].draw(in: frame2)
+            let frame2 = CGRect(x: width/4, y: 0, width: width/2, height: height)
+            listImage[1].draw(in: frame2)
+            UIGraphicsGetCurrentContext()?.resetClip()
+            
+            let borderPath = UIBezierPath()
+            borderPath.move(to: CGPoint(x: width/4, y: 0))
+            borderPath.addLine(to: CGPoint(x: 7*width/20, y: height))
+            borderPath.move(to: CGPoint(x: width*3/4, y: 0))
+            borderPath.addLine(to: CGPoint(x: 13*width/20, y: height))
+            UIColor.white.setStroke()
+            borderPath.lineWidth = 4
+            borderPath.stroke()
+            
+            UIColor.systemTeal.setFill()
+            UIBezierPath(rect: CGRect(origin: .init(x: .zero, y: height), size: .init(width: width, height: textBoundHeight))).fill()
+            
+            let listAttributedText = listText
+            listAttributedText[0].draw(in: .init(x: 0, y: height, width: width/3, height: textBoundHeight))
+            listAttributedText[1].draw(in: .init(x: width/3, y: height, width: width/3, height: textBoundHeight))
+            listAttributedText[2].draw(in: .init(x: width * 2/3, y: height, width: width/3, height: textBoundHeight))
         }
         finalImage = img
         finalImageView.image = img
+    }
+    func resetData(){
+        for imageView in chosenImageViews {
+            imageView.image = nil
+        }
+        
+        // Reset text field
+        for textField in nameTextFields {
+            textField.text = ""
+        }
+        
+        // Reset biến dữ liệu
+        listImage.removeAll()
+    }
+    @IBAction func add(_ sender: UIButton) {
+        resetData()
+        var configuration = PHPickerConfiguration()
+        configuration.filter = .images
+        configuration.selectionLimit = maxImageCount
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = self
+        present(picker, animated: true)
+        
+    }
+    @IBAction func createPoster(_ sender: Any) {
+        drawImage(finalImageView: self.finalImageView)
+        resetData()
+    }
+    @IBAction func save(_ sender: Any) {
+        if let image = finalImage{
+            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+            finalImageView.image = nil
+            finalImage = nil
+        }
+    }
+    @IBAction func takeScreenShot(_ sender: Any) {
+        openCamera()
+    }
+    
+    @objc func hideKeyboard() {
+        view.endEditing(true)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            listImage.append(image)
+            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        }else{
+            print("Không lấy được ảnh từ thư viện")
+        }
+        
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
     }
 }
 extension ImagePickerVC: PHPickerViewControllerDelegate{
     func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
         dismiss(animated: true)
-        var listImage: [UIImage] = []
+        //        var listImage: [UIImage] = Array(repeating: UIImage(), count: results.count)
         for i in 0..<results.count{
-            var res = results[i]
+            let res = results[i]
             if(res.itemProvider.canLoadObject(ofClass: UIImage.self)){
                 res.itemProvider.loadObject(ofClass: UIImage.self) { [weak self]image, error in
-                    listImage.append(image as! UIImage)
-                    if(listImage.count == self?.numberOfImage){
-                        DispatchQueue.main.sync {
-                            self?.listImage = listImage
-                        }
+                    //                    listImage[i] = image as! UIImage
+                    //                    if(listImage.count == self?.numberOfImage){
+                    DispatchQueue.main.sync {
+                        self?.listImage.append(image as! UIImage)
                     }
+                    //                    }
                 }
             }
         }
-//        results.forEach { res in
-//            if(res.itemProvider.canLoadObject(ofClass: UIImage.self)){
-//                res.itemProvider.loadObject(ofClass: UIImage.self) { [weak self]image, error in
-//                    listImage.append(image as! UIImage)
-//                    if(listImage.count == self?.numberOfImage){
-//                        DispatchQueue.main.sync {
-//                            self?.listImage = listImage
-//                        }
-//                    }
-//                }
-//            }
-//        }
     }
 }
