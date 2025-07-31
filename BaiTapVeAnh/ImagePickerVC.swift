30a38b323784202ebbc32d32055fda7ec5f43f6e
Admin <admin@MacBook-Pro-cua-Admin.local>
2025-07-31 08:14:52 +0700

    commit

--- BaiTapVeAnh/ImagePickerVC.swift
+++ BaiTapVeAnh/ImagePickerVC.swift
@@ -12,6 +12,7 @@ class ImagePickerVC: UIViewController{
     @IBOutlet var chosenImageViews: [UIImageView]!
 //    @IBOutlet weak var chosenImageView: UIImageView!
     var maxImageCount: Int = 100
+    var index:Int = .zero
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -27,50 +28,50 @@ class ImagePickerVC: UIViewController{
     }
 }
 //extension delega
-//extension ImagePickerVC: PHPickerViewControllerDelegate{
+extension ImagePickerVC: PHPickerViewControllerDelegate{
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        dismiss(animated: true)
+        results.forEach { res in
+            if(res.itemProvider.canLoadObject(ofClass: UIImage.self)){
+                res.itemProvider.loadObject(ofClass: UIImage.self) { [weak self]image, error in
+//                    DispatchQueue.main.sync {
+//                        self?.chosenImageViews.append(image as! UIImageView)
+                        self?.chosenImageViews[self!.index].image = image as? UIImage
+                        self?.index += 1
+//                    }
+                }
+            }
+        }
+        self.index = 0
+        //        if let itemProvider = results.last?.itemProvider, itemProvider.canLoadObject(ofClass: UIImage.self){
+//            let previousImage = chosenImageViews?.image
+//            itemProvider.loadObject(ofClass: UIImage.self) {[weak self] image, error in
+//                DispatchQueue.main.async {
+//                    guard let self = self,
+//                          let image = image as? UIImage, self.chosenImageView.image == previousImage else{
+//                        return
+//                    }
+//                    self.chosenImageView.image = image
+//                }
+//            }
+//        }
+    }
+}
+//extension ImagePickerVC: PHPickerViewControllerDelegate {
 //    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
 //        dismiss(animated: true)
-////        for res in results{
-////            self.chosenImageViews.append(res)
-////        }
-//        results.forEach { res in
-//            if(res.itemProvider.canLoadObject(ofClass: UIImage.self)){
-//                res.itemProvider.loadObject(ofClass: UIImage.self) { [weak self]image, error in
+//
+//        for (index, res) in results.enumerated() {
+//            if res.itemProvider.canLoadObject(ofClass: UIImage.self) {
+//                res.itemProvider.loadObject(ofClass: UIImage.self) { [weak self] image, error in
+//                    guard let self = self, let uiImage = image as? UIImage else { return }
 //                    DispatchQueue.main.async {
-//                        self?.chosenImageViews.append(image as! UIImageView)
+//                        if index < self.chosenImageViews.count {
+//                            self.chosenImageViews[index].image = uiImage
+//                        }
 //                    }
 //                }
 //            }
 //        }
-//        //        if let itemProvider = results.last?.itemProvider, itemProvider.canLoadObject(ofClass: UIImage.self){
-////            let previousImage = chosenImageViews?.image
-////            itemProvider.loadObject(ofClass: UIImage.self) {[weak self] image, error in
-////                DispatchQueue.main.async {
-////                    guard let self = self,
-////                          let image = image as? UIImage, self.chosenImageView.image == previousImage else{
-////                        return
-////                    }
-////                    self.chosenImageView.image = image
-////                }
-////            }
-////        }
 //    }
 //}
-extension ImagePickerVC: PHPickerViewControllerDelegate {
-    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-        dismiss(animated: true)
-
-        for (index, res) in results.enumerated() {
-            if res.itemProvider.canLoadObject(ofClass: UIImage.self) {
-                res.itemProvider.loadObject(ofClass: UIImage.self) { [weak self] image, error in
-                    guard let self = self, let uiImage = image as? UIImage else { return }
-                    DispatchQueue.main.async {
-                        if index < self.chosenImageViews.count {
-                            self.chosenImageViews[index].image = uiImage
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
