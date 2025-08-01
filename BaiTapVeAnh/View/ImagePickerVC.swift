bbfec582cf0eb3e7096312b3fd7e1c64847c9e66
Admin <admin@MacBook-Pro-cua-Admin.local>
2025-08-01 23:21:03 +0700

    update

--- BaiTapVeAnh/View/ImagePickerVC.swift
+++ BaiTapVeAnh/View/ImagePickerVC.swift
@@ -91,23 +91,52 @@ class ImagePickerVC: UIViewController, UIImagePickerControllerDelegate & UINavig
     }
     func drawImageWithoutDistorted(_ image: UIImage, in frame: CGRect){
         let imageAspect = image.size.width / image.size.height
+            let frameAspect = frame.width / frame.height
+            
+            var drawRect = frame
+            if imageAspect > frameAspect {
+                // Ảnh rộng hơn → crop 2 bên
+                let scale = frame.height / image.size.height
+                let width = image.size.width * scale
+                drawRect.origin.x = frame.midX - width / 2
+                drawRect.size.width = width
+            } else {
+                // Ảnh cao hơn → crop trên dưới
+                let scale = frame.width / image.size.width
+                let height = image.size.height * scale
+                drawRect.origin.y = frame.midY - height / 2
+                drawRect.size.height = height
+            }
+            image.draw(in: drawRect)
+    }
+    func drawImageEdgeAspectFill(_ image: UIImage, in frame: CGRect) {
+        let imageAspect = image.size.width / image.size.height
         let frameAspect = frame.width / frame.height
 
         var drawRect = frame
         if imageAspect > frameAspect {
-            // Ảnh rộng hơn khung → scale theo chiều dọc, crop 2 bên
             let scale = frame.height / image.size.height
             let width = image.size.width * scale
-            drawRect.origin.x += (frame.width - width) / 2
+            drawRect.origin.x = frame.midX - width / 2
             drawRect.size.width = width
         } else {
-            // Ảnh cao hơn khung → scale theo chiều ngang, crop trên dưới
             let scale = frame.width / image.size.width
             let height = image.size.height * scale
-            drawRect.origin.y += (frame.height - height) / 2
+            drawRect.origin.y = frame.midY - height / 2
             drawRect.size.height = height
         }
         image.draw(in: drawRect)
+    }
+
+    func drawPathAroundImage(startPoint: CGPoint,linePoint1: CGPoint, linePoint2: CGPoint, linePoint3: CGPoint, i:Int, path: UIBezierPath){
+//        let path = UIBezierPath()
+//        let iCGFLoat = CGFloat(i)
+        path.move(to: startPoint)
+        path.addLine(to: linePoint1)
+        path.addLine(to: linePoint2)
+        path.addLine(to: linePoint3)
+        path.close()
+        path.addClip()
     }
     func drawImage(finalImageView: UIImageView, numOfImage: Int){
         let frame = finalImageView.bounds
@@ -123,16 +152,37 @@ class ImagePickerVC: UIViewController, UIImagePickerControllerDelegate & UINavig
             let img = renderer.image { ctx in
                 for i in 0..<numOfImage{
                     if(i%2 == 0){
+                        let path =  UIBezierPath()
+                        let iCGFloat = CGFloat(i)
+                        let numOfImageCGFloat:CGFloat = CGFloat(numOfImage)
+                        print(i)
                         if(i == 0){
-                            let width = singleImageWidth+CGFloat(additionWidth)
+                            let startPoint = CGPoint(x: (iCGFloat*width/numOfImageCGFloat), y: 0)
+                            let linePoint1 = CGPoint(x: (iCGFloat*singleImageWidth), y: height)
+                            let linePoint2 = CGPoint(x: ((iCGFloat+1)*singleImageWidth)+(CGFloat(additionWidth)), y: height)
+                            let linePoint3 = CGPoint(x: ((iCGFloat+1)*singleImageWidth)+(CGFloat(additionWidth)), y: 0)
+                            drawPathAroundImage(startPoint: startPoint, linePoint1: linePoint1, linePoint2: linePoint2, linePoint3: linePoint3, i: i,path: path)
                             let frame = CGRect(x: width*CGFloat(i)/CGFloat(numOfImage), y: 0, width: singleImageWidth+CGFloat(additionWidth), height: height)
-                            listImage[i].draw(in: frame)
+                            drawImageWithoutDistorted(listImage[i], in: frame)
+                            UIGraphicsGetCurrentContext()?.resetClip()
                         }else if(i == numOfImage-1){
+                            let startPoint = CGPoint(x: (iCGFloat*width/numOfImageCGFloat)-CGFloat(additionWidth), y: 0)
+                            let linePoint1 = CGPoint(x: (iCGFloat*singleImageWidth)-CGFloat(additionWidth), y: height)
+                            let linePoint2 = CGPoint(x: ((iCGFloat+1)*singleImageWidth)+(CGFloat(additionWidth)), y: height)
+                            let linePoint3 = CGPoint(x: ((iCGFloat+1)*singleImageWidth)+(CGFloat(additionWidth)), y: 0)
+                            drawPathAroundImage(startPoint: startPoint, linePoint1: linePoint1, linePoint2: linePoint2, linePoint3: linePoint3, i: i,path:path)
                             let frame = CGRect(x: CGFloat(i)*width/CGFloat(numOfImage) - additionWidth, y: 0, width: singleImageWidth+CGFloat(additionWidth), height: height)
-                            listImage[i].draw(in: frame)
+                            drawImageWithoutDistorted(listImage[i], in: frame)
+                            UIGraphicsGetCurrentContext()?.resetClip()
                         }else{
+                            let startPoint = CGPoint(x: (iCGFloat*width/numOfImageCGFloat)-(CGFloat(additionWidth)), y: 0)
+                            let linePoint1 = CGPoint(x: (iCGFloat*singleImageWidth)-(CGFloat(additionWidth)), y: height)
+                            let linePoint2 = CGPoint(x: ((iCGFloat+1)*singleImageWidth)+2*(CGFloat(additionWidth)), y: height)
+                            let linePoint3 = CGPoint(x: ((iCGFloat+1)*singleImageWidth)+2*(CGFloat(additionWidth)), y:0)
+                            drawPathAroundImage(startPoint: startPoint, linePoint1: linePoint1, linePoint2: linePoint2, linePoint3: linePoint3, i: i,path:path)
                             let frame = CGRect(x: CGFloat(i)*width/CGFloat(numOfImage)-additionWidth, y: 0, width: singleImageWidth+2*additionWidth, height: height)
-                            listImage[i].draw(in: frame)
+                            drawImageWithoutDistorted(listImage[i], in: frame)
+                            UIGraphicsGetCurrentContext()?.resetClip()
                         }
                     }
                 }
@@ -141,13 +191,11 @@ class ImagePickerVC: UIViewController, UIImagePickerControllerDelegate & UINavig
                     let iCGFloat = CGFloat(i)
                     let numOfImageCGFloat:CGFloat = CGFloat(numOfImage)
                     if(i % 2 != 0){
-                        print(i)
-                        path.move(to: CGPoint(x: (iCGFloat*width/numOfImageCGFloat)-(CGFloat(additionWidth)), y: 0))
-                        path.addLine(to: CGPoint(x: (iCGFloat*singleImageWidth)+(CGFloat(additionWidth)), y: height))
-                        path.addLine(to: CGPoint(x: ((iCGFloat+1)*singleImageWidth)-(CGFloat(additionWidth)), y: height))
-                        path.addLine(to: CGPoint(x: ((iCGFloat+1)*singleImageWidth)+(CGFloat(additionWidth)), y: 0))
-                        path.close()
-                        path.addClip()
+                        let startPoint = CGPoint(x: (iCGFloat*width/numOfImageCGFloat)-(CGFloat(additionWidth)), y: 0)
+                        let linePoint1 = CGPoint(x: (iCGFloat*singleImageWidth)+(CGFloat(additionWidth)), y: height)
+                        let linePoint2 = CGPoint(x: ((iCGFloat+1)*singleImageWidth)-(CGFloat(additionWidth)), y: height)
+                        let linePoint3 = CGPoint(x: ((iCGFloat+1)*singleImageWidth)+(CGFloat(additionWidth)), y: 0)
+                        drawPathAroundImage(startPoint: startPoint, linePoint1: linePoint1, linePoint2: linePoint2, linePoint3: linePoint3, i: i,path: path)
                         let frame = CGRect(x: CGFloat(i)*width/CGFloat(numOfImage)-additionWidth, y: 0, width: width/CGFloat(numOfImage)+2*additionWidth, height: height)
                         drawImageWithoutDistorted(listImage[i], in: frame)
                         UIGraphicsGetCurrentContext()?.resetClip()
@@ -163,14 +211,28 @@ class ImagePickerVC: UIViewController, UIImagePickerControllerDelegate & UINavig
             let img = renderer.image { ctx in
                 for i in 0..<numOfImage{
                     if(i%2 == 0){
+                        let path =  UIBezierPath()
+                        let iCGFloat = CGFloat(i)
+                        let numOfImageCGFloat:CGFloat = CGFloat(numOfImage)
                         if(i == 0){
-                            let width = singleImageWidth+CGFloat(additionWidth)
-                            let frame = CGRect(x: CGFloat(i)*width/CGFloat(numOfImage), y: 0, width: singleImageWidth+CGFloat(additionWidth), height: height)
-                            listImage[i].draw(in: frame)
+                            let startPoint = CGPoint(x: (iCGFloat*width/numOfImageCGFloat), y: 0)
+                            let linePoint1 = CGPoint(x: (iCGFloat*singleImageWidth), y: height)
+                            let linePoint2 = CGPoint(x: ((iCGFloat+1)*singleImageWidth)+(CGFloat(additionWidth)), y: height)
+                            let linePoint3 = CGPoint(x: ((iCGFloat+1)*singleImageWidth)+(CGFloat(additionWidth)), y: 0)
+                            drawPathAroundImage(startPoint: startPoint, linePoint1: linePoint1, linePoint2: linePoint2, linePoint3: linePoint3, i: i,path: path)
+                            let frame = CGRect(x: width*CGFloat(i)/CGFloat(numOfImage), y: 0, width: singleImageWidth+CGFloat(additionWidth), height: height)
+                            drawImageWithoutDistorted(listImage[i], in: frame)
+                            UIGraphicsGetCurrentContext()?.resetClip()
                         }
                         else{
+                            let startPoint = CGPoint(x: (iCGFloat*width/numOfImageCGFloat)-(CGFloat(additionWidth)), y: 0)
+                            let linePoint1 = CGPoint(x: (iCGFloat*singleImageWidth)-(CGFloat(additionWidth)), y: height)
+                            let linePoint2 = CGPoint(x: ((iCGFloat+1)*singleImageWidth)+2*(CGFloat(additionWidth)), y: height)
+                            let linePoint3 = CGPoint(x: ((iCGFloat+1)*singleImageWidth)+2*(CGFloat(additionWidth)), y:0)
+                            drawPathAroundImage(startPoint: startPoint, linePoint1: linePoint1, linePoint2: linePoint2, linePoint3: linePoint3, i: i,path: path)
                             let frame = CGRect(x: CGFloat(i)*width/CGFloat(numOfImage)-additionWidth, y: 0, width: singleImageWidth+2*additionWidth, height: height)
-                            listImage[i].draw(in: frame)
+                            drawImageWithoutDistorted(listImage[i], in: frame)
+                            UIGraphicsGetCurrentContext()?.resetClip()
                         }
                     }
                 }
@@ -179,22 +241,20 @@ class ImagePickerVC: UIViewController, UIImagePickerControllerDelegate & UINavig
                     let iCGFloat = CGFloat(i)
                     let numOfImageCGFloat:CGFloat = CGFloat(numOfImage)
                     if(i % 2 != 0 && i != numOfImage-1){
-                        path.move(to: CGPoint(x: (iCGFloat*width/numOfImageCGFloat)-(CGFloat(additionWidth)), y: 0))
-                        path.addLine(to: CGPoint(x: (iCGFloat*singleImageWidth)+(CGFloat(additionWidth)), y: height))
-                        path.addLine(to: CGPoint(x: ((iCGFloat+1)*singleImageWidth)-(CGFloat(additionWidth)), y: height))
-                        path.addLine(to: CGPoint(x: ((iCGFloat+1)*singleImageWidth)+(CGFloat(additionWidth)), y: 0))
-                        path.close()
-                        path.addClip()
+                        let startPoint = CGPoint(x: (iCGFloat*width/numOfImageCGFloat)-(CGFloat(additionWidth)), y: 0)
+                        let linePoint1 = CGPoint(x: (iCGFloat*singleImageWidth)+(CGFloat(additionWidth)), y: height)
+                        let linePoint2 = CGPoint(x: ((iCGFloat+1)*singleImageWidth)-(CGFloat(additionWidth)), y: height)
+                        let linePoint3 = CGPoint(x: ((iCGFloat+1)*singleImageWidth)+(CGFloat(additionWidth)), y: 0)
+                        drawPathAroundImage(startPoint: startPoint, linePoint1: linePoint1, linePoint2: linePoint2, linePoint3: linePoint3, i: i,path:path)
                         let frame = CGRect(x: CGFloat(i)*width/CGFloat(numOfImage)-additionWidth, y: 0, width: width/CGFloat(numOfImage)+2*additionWidth, height: height)
                         drawImageWithoutDistorted(listImage[i], in: frame)
                         UIGraphicsGetCurrentContext()?.resetClip()
                     }else if(i == numOfImage - 1){
-                        path.move(to: CGPoint(x: (iCGFloat*width/numOfImageCGFloat)-(CGFloat(additionWidth)), y: 0))
-                        path.addLine(to: CGPoint(x: (iCGFloat*singleImageWidth)+(CGFloat(additionWidth)), y: height))
-                        path.addLine(to: CGPoint(x: ((iCGFloat+1)*singleImageWidth), y: height))
-                        path.addLine(to: CGPoint(x: ((iCGFloat+1)*singleImageWidth)+(CGFloat(additionWidth)), y: 0))
-                        path.close()
-                        path.addClip()
+                        let startPoint = CGPoint(x: (iCGFloat*width/numOfImageCGFloat)-(CGFloat(additionWidth)), y: 0)
+                        let linePoint1 = CGPoint(x: (iCGFloat*singleImageWidth)+(CGFloat(additionWidth)), y: height)
+                        let linePoint2 = CGPoint(x: ((iCGFloat+1)*singleImageWidth), y: height)
+                        let linePoint3 = CGPoint(x: ((iCGFloat+1)*singleImageWidth)+(CGFloat(additionWidth)), y: 0)
+                        drawPathAroundImage(startPoint: startPoint, linePoint1: linePoint1, linePoint2: linePoint2, linePoint3: linePoint3, i: i,path:path)
                         let frame = CGRect(x: CGFloat(i)*width/CGFloat(numOfImage)-additionWidth, y: 0, width: width/CGFloat(numOfImage)+2*additionWidth, height: height)
                         drawImageWithoutDistorted(listImage[i], in: frame)
                         UIGraphicsGetCurrentContext()?.resetClip()
@@ -227,8 +287,10 @@ class ImagePickerVC: UIViewController, UIImagePickerControllerDelegate & UINavig
         
     }
     @IBAction func createPoster(_ sender: Any) {
-        drawImage(finalImageView: self.finalImageView,numOfImage: listImage.count)
-        resetData()
+        if(!listImage.isEmpty){
+            drawImage(finalImageView: self.finalImageView,numOfImage: listImage.count)
+            resetData()
+        }
     }
     @IBAction func save(_ sender: Any) {
         if let image = finalImage{
